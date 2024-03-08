2f42b4e63bd0da01c8cf83c5765f42f760668f29
felixozcoz <801108@unizar.es>
2024-03-08 22:44:11 +0100

    Corrección bugs + optimización
    Dudas de si es correcto.
    Falta comprobar el rango para plotear.

--- Project 2 - Contornos/operators.py
+++ Project 2 - Contornos/operators.py
@@ -16,15 +16,55 @@
 import cv2
 import numpy as np
 
-def Gaussian_filter(x, sigma):
-    return np.sqrt(1 / (2 * np.pi * sigma)) * np.exp( -(x**2) / (2 * sigma**2))  # one-dimensional Gaussian filter
+def Gaussian_filter(kernel_size, sigma):
+    '''
+        Implement the Gaussian filter
+
+        Parameters:
+            kernel_size (int): Size of the kernel
+            sigma (float): Standard deviation of the Gaussian filter
+        
+        Returns:
+            gfx (numpy array): Gaussian filter in the x direction
+    '''
+    # create an array from -kernel_size/2 to kernel_size/2
+    x = np.arange(-(kernel_size // 2), kernel_size // 2 + 1)
+
+    gaussian_filter_1D_equation = lambda x, sigma : (1 / (np.sqrt(2*np.pi) * sigma)) * np.exp( -x**2 / (2 * sigma**2))
+
+    # calculate the Gaussian filter
+    gfx = ( -1 * gaussian_filter_1D_equation(x, sigma) ).reshape(1, kernel_size)  # x direction
+    gfy = gfx.reshape(kernel_size, 1)                                        # y direction       
+
+    return gfx, gfy
 
 
-def Gaussian_first_derivative(x, sigma):
-    return ((-x) / (sigma**2)) * np.exp((-x**2) / (2 * sigma**2)) # First derivative of the 1D Gaussian filter formula
+def Gaussian_first_derivative(kernel_size, sigma):
+    '''
+        Implement the first derivative of the Gaussian filter
+
+        Parameters:
+            kernel_size (int): Size of the kernel
+            sigma (float): Standard deviation of the Gaussian filter
+        
+        Returns:
+            gfdx (numpy array): First derivative of the Gaussian filter in the x direction
+            gfdy (numpy array): First derivative of the Gaussian filter in the y direction
+    '''
+    # create an array from -kernel_size/2 to kernel_size/2
+    x = np.arange(-(kernel_size // 2), kernel_size // 2 + 1)
+
+    gaussian_first_derivative_eq = lambda x, sigma : ((-x) / (sigma**2)) * np.exp((-x**2) / (2 * sigma**2))
+
+    # calculate the first derivative of the Gaussian filter
+    gfdx = ( -1 * gaussian_first_derivative_eq(x, sigma) ).reshape(1, kernel_size)  # x direction
+    gfdy = gfdx.reshape(kernel_size, 1)[::-1]                                             # y direction                 
+
+    return gfdx, gfdy
 
 
-def Canny_operator(image, sigma, kernel_size=5):
+
+def Canny_operator(image, kernel_size=3, sigma=1):
     '''
         Implement the Canny operator
 
@@ -39,53 +79,46 @@ def Canny_operator(image, sigma, kernel_size=5):
             theta (numpy array): Gradient orientation
     '''
     # Construir kernel 1D 
+    gfx, gfy = Gaussian_filter(kernel_size, sigma)    # Gaussian filter in the x direction
+    gfdx, gfdy = Gaussian_first_derivative(kernel_size, sigma) # First derivative of the Gaussian filter in the x direction
 
-    
-
-    x1 = np.zeros((1, kernel_size), dtype=np.float32)
-    x2 = np.zeros((kernel_size, 1), dtype=np.float32)
-
-    y1 = np.zeros((1, kernel_size), dtype=np.float32)
-    y2 = np.zeros((kernel_size, 1), dtype=np.float32)
-
-    center = kernel_size // 2
+    print(gfdx)
+    print(gfdy)
 
 
-    
-
-    # Generar kernel 2D Gaussian Filter
-    for x in range(kernel_size):
-        for y in range(kernel_size):
-            dx = x - center
-            dy = y - center
-            x1[0, x] = -1 * Gaussian_first_derivative(dx, sigma)
-            x2[y, 0] = Gaussian_filter(dy, sigma)
-
-            y1[0, x] = Gaussian_filter(dx, sigma)
-            y2[y, 0] = Gaussian_first_derivative(dy, sigma)
-
-    
-    print(x1)
-    print(x2)
-    print(y1)
-    print(y2)
-
-    kernelX = np.dot(x2, x1)
-    kernelY = np.dot(y2, y1)
+    kernelX = np.dot(gfy, gfdx) # kernel in the x direction
+    kernelY = np.dot(gfdy, gfx) # kernel in the y direction
 
     print(kernelX)
     print(kernelY)
 
-    # aplicar los kernels a la imagen con filter 2D
+    # normalize the kernels
+    K = np.sum(kernelX[kernelX > 0]) 
+    kernelX = kernelX / K
+    kernelY = kernelY / K
 
-    return grad_x, grad_y
+    # apply the kernels to the image
+    Gx = cv2.filter2D(image, -1, kernelX)
+    Gy = cv2.filter2D(image, -1, kernelY)
+
+    # TODO: posible ajuste de rango de color
+    print(np.min(Gx), np.max(Gx))
+    print(np.min(Gy), np.max(Gy))
+
+    return Gx, Gy
 
 
-    
 
 
-img = cv2.imread(r"C:\Users\felix\OneDrive\Escritorio\chess_table.jpg", cv2.IMREAD_GRAYSCALE)
-Gx, Gy = Canny_operator(img, 1, 5)
+
+img = cv2.imread(r"C:\Users\felix\OneDrive\Escritorio\poster.pgm", cv2.IMREAD_GRAYSCALE)
+
+kernel_size = 5
+sigma = 1
+
+Gx, Gy = Canny_operator(img, kernel_size, sigma)
+cv2.imshow("Original", img)
 cv2.imshow("Gx", Gx)
 cv2.imshow("Gy", Gy)
 cv2.waitKey(0)
+    
