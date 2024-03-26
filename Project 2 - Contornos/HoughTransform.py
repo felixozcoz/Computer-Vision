2215c513ed66ad2b079d76ccc00360ec9ae47e23
felixozcoz <801108@unizar.es>
2024-03-26 13:55:18 +0100

    Casi terminado, pero funcional

--- Project 2 - Contornos/HoughTransform.py
+++ Project 2 - Contornos/HoughTransform.py
@@ -81,35 +81,46 @@ def hough(src):
         
         # print( "j=", j, "i=", i, "j_real=",j_real, "rho=", rho)#, "theta=", theta)
         # continue
-        aux = src.copy()
+        # aux = src.copy()
         # cv.circle(src, (j, i), 1, (0, 0, 255), 1)
         if 0 <= j_aprox < w:
             # print("i=", i, "j=", j, "j_aprox=", j_aprox, "x_to_find=", x_to_find, "rho=", rho, "cos=", th_cos, "sin=", th_sin, "x=", x, "y=", y, "theta=", theta, "theta_deg=", np.degrees(theta))
-            aux = src.copy()
-            for j_var in range(w):
-                x_var = j_var - w/2
-                y_var = int((rho - (x_var * th_cos)) / th_sin)
-                i_var = int(h/2 - y_var)
-                if 0 <= i_var < h: 
-                    cv.circle(aux, (j_var, i_var), 1, (0, 255, 0), 1)
+            # aux = src.copy()
+            # for j_var in range(w):
+            #     x_var = j_var - w/2
+            #     y_var = int((rho - (x_var * th_cos)) / th_sin)
+            #     i_var = int(h/2 - y_var)
+            #     if 0 <= i_var < h: 
+            #         cv.circle(aux, (j_var, i_var), 1, (0, 255, 0), 1)
 
             # cv.circle(src, (j, i), 1, (0, 255, 0), 1)
-            cv.circle(aux, (j, i), 2, (0, 0, 255), 2)
-            cv.circle(aux, (j_aprox, h//2), 2, (255, 0, 0), 2)
-            cv.imshow("aux", aux)
-            cv.waitKey(0)
+            # cv.circle(aux, (j, i), 2, (0, 0, 255), 2)
+            # cv.circle(aux, (j_aprox, h//2), 2, (255, 0, 0), 2)
+            # cv.imshow("aux", aux)
+            # cv.waitKey(0)
             # Recta vota por punto j
             accumulator[j_aprox] += 1
             pts[j_aprox].add((j,i))
     
-    median = np.median(accumulator)
-    max = np.argmax(accumulator)
-    for i in range(len(accumulator)):
-        if accumulator[i] > median:
-            cv.circle(src, (i, h//2), 1, (255, 0, 0), 1)    # Puntos más votados
-    cv.circle(src, (max, h//2), 1, (0, 0, 255), 1)  # Punto más votado
 
-    # print(img.shape)
+    ind, val = k_maximos_con_indices(accumulator, 5)
+    print(ind, val)
+
+    for i in ind:
+        cv.circle(src, (i, h//2), 2, (0, 255, 0), 2)    # Puntos más votados
+
+    # median = np.median(accumulator)
+    # max = np.argmax(accumulator)
+    # for i in range(len(accumulator)):
+    #     if accumulator[i] > median:
+    #         cv.circle(src, (i, h//2), 1, (255, 0, 0), 1)    # Puntos más votados
+    max_ind = ind[0]
+    for p in pts[max_ind]:
+        cv.line(src, (p[0], p[1]), (max_ind, h//2), (255, 0, 0), 1)
+        cv.circle(src, p, 2, (0, 0, 255), 2)    # puntos que votaron por la recta más votada
+
+    cv.circle(src, (max_ind, h//2), 2, (0, 0, 0), 2)  # Punto más votado
+
     return src
 
 def k_maximos_con_indices(arreglo, k):
@@ -124,18 +135,13 @@ def k_maximos_con_indices(arreglo, k):
     
     return k_indices_maximos, k_valores_maximos
 
-src1 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\Contornos\pasillo2.pgm")
+src2 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\Contornos\pasillo2.pgm")
 # src1 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\oblicua.png")
-src1 = hough(src1)
-# cv.imwrite(r"C:\Users\felix\OneDrive\Escritorio\result_prueba.png", src1)
+src2 = hough(src2)
 
-# src2 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\Contornos\pasillo2.pgm")
-# src2 = hough(src2)
+src3 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\Contornos\pasillo3.pgm")
+src3 = hough(src3)
 
-# src3 = cv.imread(r"C:\Users\felix\OneDrive\Escritorio\Contornos\pasillo3.pgm")
-# src3 = hough(src3)
-
-cv.imshow("Pasillo 1", src1)
-# cv.imshow("Pasillo 2", src2)
-# cv.imshow("Pasillo 3", src3)
+cv.imshow("Pasillo 2", src2)
+cv.imshow("Pasillo 3", src3)
 cv.waitKey(0)
